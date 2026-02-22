8228121cfe2f0c0ed66f7eff7dccc85cf8a1181a
jame25 <jame25@users.noreply.github.com>
2026-02-22 06:16:50 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,12 @@
 
 DECLARE_COMPONENT_VERSION(
     "Artwork Grabber",
-    "1.0.1",
+    "1.0.2",
     "Browse and download album artwork from multiple APIs.\n"
     "Click download arrow in foo_artwork panel to access."
 );
 
 VALIDATE_COMPONENT_FILENAME("foo_artgrab.dll");
 FOOBAR2000_IMPLEMENT_CFG_VAR_DOWNGRADE;
+
 
