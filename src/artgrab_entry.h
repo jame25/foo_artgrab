6b16b7bb6b83ec378cdfca9e16a1014be1d9af58
jame25 <jame25@users.noreply.github.com>
2026-02-22 17:22:49 +0000

    Update artgrab_entry.h

--- src/artgrab_entry.h
+++ src/artgrab_entry.h
@@ -8,6 +8,8 @@ namespace artgrab {
     pfc::string8 get_effective_discogs_consumer_key();
     pfc::string8 get_effective_discogs_consumer_secret();
     bool is_api_enabled(const char* api_name);
+    void invalidate_artwork_cache(const char* artist, const char* track);
+    void refresh_artwork_panel();
     void initialize();
     void shutdown();
 }
